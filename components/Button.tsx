c4f94bfce5848ce78396165b76e35165e3c32f78
Shrutika Jaiswal <jaiswalshrutika19@gmail.com>
2024-05-09 08:53:00 +0530

    added component

---
+++ components/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+    return (
+        <button type="button">
+            Button
+        </button>
+    )
+}
+
+export default Button
---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+import { NAV_LINKS } from "@/constants"
+import Image from "next/image"
+import Link from "next/link"
+import Button from "./Button"
+
+const Navbar = () => {
+    return (
+        <nav className="border-2 border-red300 flexBetween max-container padding-container relative z-30 py-5">
+            <Link href='/'>
+                <Image src='/logo_transparent.png' alt="logo" width={89} height={35} />
+            </Link>
+            <ul className="hidden h-full gap-12 lg:flex">
+                {NAV_LINKS.map((link) => (
+                    <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+                        {link.label}
+                    </Link>
+                ))}
+            </ul>
+            <div className="lg:flexCenter hidden">
+                <Button />
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
